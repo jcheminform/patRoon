3c7dddbdcbc0ab10e4c3a313ff0582db39cb3af7
Rick Helmus <r.helmus@uva.nl>
2019-09-12 12:09:01 +0200

    suspect export

--- R/generics.R
+++ R/generics.R
@@ -79,6 +79,7 @@ setGeneric("plotHeatMap", function(obj, ...) standardGeneric("plotHeatMap"))
 setGeneric("suspects", function(pred) standardGeneric("suspects"))
 setGeneric("predictions", function(pred) standardGeneric("predictions"))
 setGeneric("convertToMFDB", function(pred, out) standardGeneric("convertToMFDB"))
+setGeneric("convertToSuspects", function(pred, adduct, tidy = TRUE) standardGeneric("convertToSuspects"))
 
 ### suspect screening
 
